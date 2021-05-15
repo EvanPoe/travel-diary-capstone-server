54d800466a51ee987736c20a357532a9f8288f30
Evan Poe <evanpoe91@yahoo.com>
2021-05-14 19:12:56 -0500

    changed DB_URL to DATABASE_URL for heroku

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  // DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/travel-diary-capstone',
-  DB_URL: process.env.DB_URL || 'postgres://ukoavhfaizkevf:633449cc4934d1f07ff62501a3250b6f7cc4b8d6a286f2607288aaeb51bda0f1@ec2-34-204-121-199.compute-1.amazonaws.com:5432/d7l2kouukliq9h',
+  // DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/travel-diary-capstone',
+  DATABASE_URL: process.env.DATABASE_URL || 'postgres://ukoavhfaizkevf:633449cc4934d1f07ff62501a3250b6f7cc4b8d6a286f2607288aaeb51bda0f1@ec2-34-204-121-199.compute-1.amazonaws.com:5432/d7l2kouukliq9h',
   JWT_SECRET: process.env.JWT_SECRET || 'dog'
 } //what is the rule in which we convert the password into its encrypted form
 //encrypt JW token with 'dog' -- encryption key
--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 const app = require('./app')
-const { PORT, DB_URL } = require('./config')
+const { PORT, DATABASE_URL } = require('./config')
 const knex = require('knex')
 
 //tell knex how to connect to db, using the client (postgres) 
 //and the url for the databse
 const db = knex({ 
   client: 'pg',
-  connection: DB_URL
+  connection: DATABASE_URL
 })
 
 app.set('db', db)
