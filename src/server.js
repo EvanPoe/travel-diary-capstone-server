ae26626a90e7d9161065ccce7976795abf89dd89
Evan Poe <evanpoe91@yahoo.com>
2022-02-03 17:24:17 -0600

    Revert "trying ssl fix"
    This reverts commit 2e7a69e565bc0aca448b526f1ba9f81ef71ea99a.

--- src/server.js
+++ src/server.js
@@ -9,10 +9,7 @@ pg.defaults.ssl = process.env.NODE_ENV === "production";
 //and the url for the databse
 const db = knex({ 
   client: 'pg',
-  connection: DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  connection: DATABASE_URL
 })
 
 app.set('db', db)
